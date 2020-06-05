42dcfbc40ed12300c8d7ac42b108189dc56a0af3
ampaire <femayapr@gmail.com>
2020-06-05 16:38:50 -0600

    Add bootstrap

--- app/javascript/components/Home/jumbotron.js
+++ app/javascript/components/Home/jumbotron.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.css";
 
 const Jumbotron = () => {
   return (
     <section className="home-section">
       <div className="container">
         <div className="row">
-          <div className="col col-sm-12 col md-5">
+          <div className="col col-sm-12 col-md-5">
             <div className="pt-4 mt-4">
-              <h4 className="title">
-                The Complete React Web Developer Course
-              </h4>
-              <p> 2 Hour Course Preview for beginners</p>
+              <h4 className="title">Learn React</h4>
+              <p>
+                {" "}
+                The Complete React Web Developer Course.2 Hour Course Preview for
+                beginners
+              </p>
             </div>
           </div>
-          <div className="col col-sm-12 col md-7">
-            <div className="mt-4 pt-4 text-right">
+          <div className="col col-sm-12 col-md-7">
+            <div className="mt-4 pt-4 text-center">
               <iframe
                 width="300"
                 height="215"
