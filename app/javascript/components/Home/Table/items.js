3fb3163bbb322a5c63fa52608427cde66f87a24c
ampaire <femayapr@gmail.com>
2020-06-06 16:38:23 -0600

    Basic items component setup

--- app/javascript/components/Home/Table/items.js
+++ app/javascript/components/Home/Table/items.js
@@ -1,13 +1,20 @@
 import React, { Component} from 'react'
 
-class Items extends Component{
-  render() {
-    return (
-      <div>
-        
+const Items = (props) => {
+  return(
+    <div className= 'row'>
+      <div className = 'col-md-10 offset-1'>
+        <div className = 'text-center'>
+          <div className = 'card'>
+            <div className = 'row'>
+              <div className = 'col-md-4'></div>
+              <div className = 'col-md-8'></div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Items
