f2b39d41aedadb8adc6d70a012834fe9516eb9cd
ampaire <femayapr@gmail.com>
2020-06-06 16:41:25 -0600

    Add video thumbnail

---
+++ app/javascript/components/Home/Table/video.js
@@ -0,0 +1,14 @@
+const Video = (props) =>{
+  return(
+    <div>
+      <iframe
+        width="400"
+        height="215"
+        src="https://www.youtube.com/embed/qI43yX1QeLM"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen>
+      </iframe>
+    </div>
+  )
+}
